ce35aaa119c95ee0665a04e00896e3989b6dcde4
iamharnad <haranathpriyan@gmail.com>
2022-04-07 02:48:48 +0530

    Quiz-final
    Added Api to generate questions and score. Added Dummy Navigation Links. Use Context to generate data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import './App.css';
 import SetupForm from './Form/SetupForm';
-import LoadingScreen from './Loading/LoadingScreen';
-import Modal from './Modal/Modal';
-
+import Loading from './Loading/LoadingScreen';
+import Modal from './Modal/Modal.jsx';
+import { useGlobalContext } from './Context/Context.jsx';
+import Navbar from './Navbar';
 function App() {
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestions,
+    checkAnswers,
+  } = useGlobalContext();
+  if (waiting) {
+    return <SetupForm />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+
+  const { incorrect_answers, correct_answer, question } = questions[index];
+  let answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+  if (tempIndex === 3) {
+    answers.push(correct_answer);
+  } else {
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
+  }
   return (
-    <div className='App'>
-      <main>
-        <section className='quiz'>
-          <p className='correct-answers'>correct answers: 5</p>
-          <article className='container'>
-            <h2>text</h2>
-            <div className='btn-container'></div>
-          </article>
-          <button className='next-question'>next question</button>
-        </section>
-      </main>
-    </div>
+    <main>
+      <Modal />
+
+      <section className='quiz'>
+        {console.log(questions)}
+        <p className='correct-answers'>
+          correct answers: {correct}/{index}
+        </p>
+        <article className='container'>
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className='btn-container'>
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className='answer-btn'
+                  onClick={() => checkAnswers(correct_answer === answer)}
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className='next-question' onClick={nextQuestions}>
+          next questions
+        </button>
+      </section>
+    </main>
   );
 }
-
 export default App;
--- src/Form/SetupForm.jsx
+++ src/Form/SetupForm.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-
+import { useGlobalContext } from '../Context/Context';
 const SetupForm = () => {
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
   return (
     <main>
       <section className='quiz quiz-small'>
         <form className='setup-form'>
-          <h2>setup quiz</h2>
+          <h2>Trivi Quiz</h2>
           <div className='form-control'>
             <label htmlFor='amount'>number of questions</label>
             <input
               type='number'
               name='amount'
               id='amount'
+              value={quiz.amount}
+              onChange={handleChange}
               className='form-input'
               min={1}
               max={50}
@@ -19,30 +21,47 @@ const SetupForm = () => {
           </div>
           <div className='form-control'>
             <label htmlFor='category'>category</label>
-            <select name='category' id='category' className='form-input'>
-              <option value='sports'>sports</option>
-              <option value='history'>history</option>
-              <option value='politics'>politics</option>
+            <select
+              name='category'
+              id='category'
+              className='form-input'
+              value={quiz.category}
+              onChange={handleChange}
+            >
+              <option value='sports'>Sports</option>
+              <option value='history'>History</option>
+              <option value='politics'>Politics</option>
+              <option value='films'>Films</option>
             </select>
           </div>
           <div className='form-control'>
             <label htmlFor='difficulty'>difficulty</label>
-            <select name='difficulty' id='difficulty' className='form-input'>
-              <option value='ease'>easy</option>
-              <option value='medium'>medium</option>
-              <option value='hard'>difficult</option>
+            <select
+              name='difficulty'
+              id='difficulty'
+              className='form-input'
+              onChange={handleChange}
+              value={quiz.difficulty}
+            >
+              <option value='ease'>Easy</option>
+              <option value='medium'>Medium</option>
+              <option value='hard'>Difficult</option>
             </select>
           </div>
-
-          <p className='error'>can't generates questions , pls try again</p>
-
-          <button type='submit' className='submit-btn'>
+          {error && (
+            <p className='error'>can't generates questions , pls try again</p>
+          )}
+          <button type='submit' className='submit-btn' onClick={handleSubmit}>
             start
           </button>
+          <br></br>
+          <p className='setup-nav'>
+            <a href='#'>Home</a> | <a href='#'>Profile</a> |{' '}
+            <a href='#'>Trivia</a> | <a href='#'>About Us</a>
+          </p>
         </form>
       </section>
     </main>
   );
 };
-
 export default SetupForm;
